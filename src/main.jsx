ee8c4d09d64193f2a38b3693c1d658face5fbf73
Loy Neil <loyneil92@gmail.com>
2026-07-16 12:33:54 +0530

    Render components based on URL query parameters

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { FullscreenDiagram, BlankBuilder } from './Swimlanediagram.jsx'
+
+const params = new URLSearchParams(window.location.search)
+const view = params.get('view')
+
+const renderForView = () => {
+  if (view === 'diagram') return <FullscreenDiagram />
+  if (view === 'builder') return <BlankBuilder />
+  return <App />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {renderForView()}
   </StrictMode>,
 )
