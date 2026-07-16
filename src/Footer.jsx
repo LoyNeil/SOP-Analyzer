42ed6d064ac83415e06a1684079610f59477b4ec
Loy Neil <loyneil92@gmail.com>
2026-07-16 12:33:11 +0530

    Update footer text to reflect new maintainer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ import FooterLogo from "./assets/Footer.webp"
 const Footer = () => {
     return(
         <footer className="flex items-center justify-center gap-2 py-4 text-xs text-[#00528d]/50">
-            Developed and Maintained by the CAMP Team
+            Developed and Maintained by Neil
             <img src={FooterLogo} alt="logo" className="w-5 h-5 object-contain"/>
         </footer>
     )
